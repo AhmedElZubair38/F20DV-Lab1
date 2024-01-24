5b5c5a3c027e314676a8daad2ee6341aac98c9da
Ahmed ElZubair <ae87@hw.ac.uk>
2024-01-24 10:59:44 +0400

    somewhere there about to finish tutorial 2

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,36 @@
 'use strict';
 
-let barContainer = d3.select('div#bar1');
+// let barContainer = d3.select('div#bar1');
 
 // let barSvg = barContainer.append('svg')
-// .attr('width', 800)
-// .attr('height', 500)
-// .attr('class', 'barchart');
+//     .attr('width', 800)
+//     .attr('height', 600)
+//     .classed('barchart', true);
 
-let barSvg = barContainer.append('svg')
-.attr('width', 800)
-.attr('height', 500)
-.classed('barchart', true);
+// let cities = [
+//     {city: 'Edinburgh', pop: 506000, area: 119, alt: 47},
+//     {city: 'Dubai', pop: 3604000, area: 1610, alt: 5},
+//     {city: 'Putrajaya', pop: 109000, area: 49, alt: 38},
+//     {city: 'Qingdao', pop: 10071000, area: 11228, alt: 25},
+//     {city: 'Lagos', pop: 8048000, area: 1171, alt: 41},
+//     {city: 'Ottawa', pop: 1017000, area: 2790, alt: 70}
+// ]
+
+// let barGroup = barSvg.append('g');
+
+// let bars = barGroup.selectAll('rect.bar')
+//     .data(cities, d=>d.city)
+//     .join('rect')
+//     .classed('bar', true)
+//     .attr('x', (d,i)=>i*40+5)
+//     .attr('y', d => 600 - d.alt * 10)
+//     .attr('width', 40)
+//     .attr('height', d=>d.alt*10)
+//     .style('fill', d=>d.pop<1000000?'#ba4a53':null)
+//     .style('stroke', d=>d.pop<1000000?'#381619':null);
+
+// in file main.js
+import BarChart from './BarChart.js';
 
 let cities = [
     {city: 'Edinburgh', pop: 506000, area: 119, alt: 47},
@@ -21,8 +41,8 @@ let cities = [
     {city: 'Ottawa', pop: 1017000, area: 2790, alt: 70}
 ]
 
-let barGroup = barSvg.append('g');
+let bar1 = new BarChart('div#bar1', 800, 750);
 
-let bars = barGroup.selectAll('rect')
-.data(cities, d=>d.city);
+let citiesElevation = cities.map(d=>[d.city, d.alt]);
 
+bar1.render(citiesElevation); 
