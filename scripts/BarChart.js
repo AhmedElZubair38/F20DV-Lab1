096fd675a49841e83233686a8a489de56096fffc
Ahmed ElZubair <ae87@hw.ac.uk>
2024-01-26 23:49:02 +0400

    ahaaa

--- scripts/BarChart.js
+++ scripts/BarChart.js
@@ -1,69 +1,163 @@
-export default class BarChart{
-    // Attributes (you can make those private too)
-    width; height; // size
-    svg; chart; bars; // selections
-    data; // internal data
-    // Constructor
-    // constructor(container, width, height){
-    // this.width = width;
-    // this.height = height;
-    // this.svg = d3.select(container).append('svg')
-    // .classed('barchart', true)
-    // .attr('width', width).attr('height', height);
-    // this.chart = this.svg.append('g');
-    // this.bars = this.chart.selectAll('rect.bar');
-    // }
+// export default class BarChart {
+
+//     width; height;
+//     svg; chart; bars;
+//     data;
+
+//     constructor(container, width, height, margin){
+        
+//         this.width = width;
+//         this.height = height;
+//         this.margin = margin;
+
+//         this.svg = d3.select(container)
+//             .append('svg')
+//             .attr('width', this.width)
+//             .attr('height', this.height)
+//             .classed('barchart', true);
+
+//         // this.chart = this.svg
+//         //     .append('g')
+//         //     .attr('transform', `translate(${this.margin[2]},${this.margin[0]})`);
+            
+//         // this.bars = this.chart.selectAll('rect.bar');
+
+//         this.chart = this.svg
+//             .append('g')
+//             .attr('transform', `translate(${this.margin[2]},${this.margin[0]})`);
+
+//         this.bars = this.chart.selectAll('rect.bar');
+
+//         this.axisX = this.svg
+//             .append('g')
+//             .attr('transform', `translate(${this.margin[2]},${this.height-this.margin[1]})`);
+
+//         this.axisY = this.svg
+//         .append('g')
+//         .attr('transform', `translate(${this.margin[2]},${this.margin[0]})`);
+
+//         this.scale = this.chart.selectAll('scale.scale');
+//     }
+
+//     #updateBars() {
+
+//     this.bars = this.bars
+//         .data(this.data, d=>d[0])
+//         .join('rect')
+//         .classed('bar', true)
+//         .attr('x', d=>this.scaleX(d[0]))
+//         .attr('width', this.scaleX.bandwidth())
+//         .attr('y', d=>this.scaleY(d[1]))
+//         .attr('height', d=>this.scaleY(0)-this.scaleY(d[1]));
+//     }
+
+//     #updateScales() {
+
+//         let chartWidth = this.width-this.margin[2]-this.margin[3],
+//             chartHeight = this.height-this.margin[0]-this.margin[1];
+
+//         let rangeX = [0, chartWidth],
+//             rangeY = [chartHeight, 0];
+
+//         let domainX = this.data.map(d=>d[0]),
+//             domainY = [0, d3.max(this.data, d=>d[1])];
+            
+//         this.scaleX = d3.scaleBand(domainX, rangeX).padding(0.2);
+//         this.scaleY = d3.scaleLinear(domainY, rangeY);
+//     }
+
+//     render(dataset) {
+
+//         this.data = dataset;
+//         this.#updateScales();
+//         this.#updateBars();
+
+//         return this;
+//     }
+// }
+
+export default class BarChart {
+
+    width; height; margin;
+    svg; chart; bars; axisX; axisY; labelX; labelY;
+    scaleX; scaleY;
+    data;
+
     constructor(container, width, height, margin){
-        this.width = width;
-        this.height = height;
-        this.margin = margin;
-        this.svg = d3.select(container)
-        .append('svg')
-        .attr('width', this.width)
-        .attr('height', this.height)
-        .classed('barchart', true);
-        this.chart = this.svg.append('g')
+
+    this.width = width;
+    this.height = height;
+    this.margin = margin;
+
+    this.svg = d3.select(container).append('svg')
+        .classed('barchart', true)
+        .attr('width', this.width).attr('height', this.height);
+    this.chart = this.svg.append('g')
         .attr('transform',
         `translate(${this.margin[2]},${this.margin[0]})`);
-        this.bars = this.chart.selectAll('rect.bar');
-        this.scale = this.chart.selectAll('scale.scale');
-        }
-    // Private methods
-    // data is in the format [[key,value],...]
-    #updateBars(){
-    this.bars = this.bars
-    .data(this.data, d=>d[0])
-    .join('rect')
-    .classed('bar', true)
-    // .attr('x', (d,i) => i*40+5)
-    // .attr('y', d => this.height - d[1]*10)
-    // .attr('width', 40)
-    // .attr('height', d => d[1]*10);
-    .attr('x', d=>this.scaleX(d[0]))
-    .attr('width', this.scaleX.bandwidth())
-    .attr('y', d=>this.scaleY(d[1]))
-    .attr('height', d=>this.scaleY(0)-this.scaleY(d[1]));
+
+    this.bars = this.chart.selectAll('rect.bar');
+    this.axisX = this.svg.append('g')
+        .attr('transform',
+        `translate(${this.margin[2]},${this.height-this.margin[1]})`);
+    this.axisY = this.svg.append('g')
+        .attr('transform',
+        `translate(${this.margin[2]},${this.margin[0]})`);
+    this.labelX = this.svg.append('text')
+        .attr('transform', `translate(${this.width/2},${this.height})`)
+        .style('text-anchor', 'middle').attr('dy',-5);
+    this.labelY = this.svg.append('text')
+        .attr('transform', `translate(0,${this.margin[0]})rotate(-90)`)
+        .style('text-anchor', 'end').attr('dy',15);
+
     }
-    // Public API
-    // The dataset parameter needs to be in a generic format,
-    // so that it works for all future data
-    // here we assume a [[k,v], ...] format for efficiency
-    #updateScales() {
+
+    #updateScales(){
+
         let chartWidth = this.width-this.margin[2]-this.margin[3],
-            chartHeight = this.height-this.margin[0]-this.margin[1];
+        chartHeight = this.height-this.margin[0]-this.margin[1];
         let rangeX = [0, chartWidth],
-            rangeY = [chartHeight, 0];
+        rangeY = [chartHeight, 0];
         let domainX = this.data.map(d=>d[0]),
-            domainY = [0, d3.max(this.data, d=>d[1])];
-            
+        domainY = [0, d3.max(this.data, d=>d[1])];
         this.scaleX = d3.scaleBand(domainX, rangeX).padding(0.2);
         this.scaleY = d3.scaleLinear(domainY, rangeY);
     }
+        
+    #updateAxes(){
 
-    render(dataset) {
+        let axisGenX = d3.axisBottom(this.scaleX),
+        axisGenY = d3.axisLeft(this.scaleY);
+        this.axisX.call(axisGenX);
+        this.axisY.call(axisGenY);
+    }
+        
+    #updateBars(){
+
+        this.bars = this.bars
+        .data(this.data, d=>d[0])
+        .join('rect')
+        .classed('bar', true)
+        .attr('x', d=>this.scaleX(d[0]))
+        .attr('height', d=>this.scaleY(0)-this.scaleY(d[1]))
+        .attr('width', this.scaleX.bandwidth())
+        .attr('y', d=>this.scaleY(d[1]));
+    }
+        
+    render(dataset){
+
         this.data = dataset;
         this.#updateScales();
         this.#updateBars();
-        return this; // to allow chaining
+        this.#updateAxes();
+        return this;
     }
+        
+    setLabels(labelX='categories', labelY='values'){
+        
+        this.labelX.text(labelX);
+        this.labelY.text(labelY);
+        return this;
+    }
+    
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,5 @@
 'use strict';
 
-// let barContainer = d3.select('div#bar1');
-
-// let barSvg = barContainer.append('svg')
-//     .attr('width', 800)
-//     .attr('height', 600)
-//     .classed('barchart', true);
-
-// let cities = [
-//     {city: 'Edinburgh', pop: 506000, area: 119, alt: 47},
-//     {city: 'Dubai', pop: 3604000, area: 1610, alt: 5},
-//     {city: 'Putrajaya', pop: 109000, area: 49, alt: 38},
-//     {city: 'Qingdao', pop: 10071000, area: 11228, alt: 25},
-//     {city: 'Lagos', pop: 8048000, area: 1171, alt: 41},
-//     {city: 'Ottawa', pop: 1017000, area: 2790, alt: 70}
-// ]
-
-// let barGroup = barSvg.append('g');
-
-// let bars = barGroup.selectAll('rect.bar')
-//     .data(cities, d=>d.city)
-//     .join('rect')
-//     .classed('bar', true)
-//     .attr('x', (d,i)=>i*40+5)
-//     .attr('y', d => 600 - d.alt * 10)
-//     .attr('width', 40)
-//     .attr('height', d=>d.alt*10)
-//     .style('fill', d=>d.pop<1000000?'#ba4a53':null)
-//     .style('stroke', d=>d.pop<1000000?'#381619':null);
-
 // in file main.js
 import BarChart from './BarChart.js';
 
@@ -41,45 +12,24 @@ let cities = [
     {city: 'Ottawa', pop: 1017000, area: 2790, alt: 70}
 ]
 
-// let bar1 = new BarChart('div#bar1', 800, 750);
-
 let bar1 = new BarChart('div#bar1', 700, 700, [10, 40, 45, 40]);
 
 let citiesElevation = cities.map(d=>[d.city, d.alt]);
 
 bar1.render(citiesElevation);
 
-// let data = [['a',12],['b',18],['c',25],['d',3]];
-// data.map(d=>d[0]); // ['a','b','c','d']
-// console.log(d3.min(data, d=>d[1])); // 3
-// console.log(d3.max(data, d=>d[1])); // 25
-// console.log(d3.extent(data, d=>d[1])); // [3,25]
-// console.log(data.map(d=>d[1]).reverse()); // [3,25,18,12]
 
-// let mySelection = d3.select("div#bar1");
-// let s1 = d3.scaleLinear([0,500],[0,400]);
-// let axis = d3.axisBottom(s1);
-// mySelection.call(axis);
 
-// let s2 = d3.scalePoint(['A','B','C','D','F','E'],[0,400]);
-// let axis2 = d3.axisRight(s2);
-// mySelection.call(axis2);
 
-// let s3 = d3.scaleLog([1,1000], [0,400]);
-// let axis3 = d3.axisLeft(s3);
-// mySelection.call(axis3);
 
-// let dates = [new Date(2016, 0, 1), new Date(2022, 0, 1)];
-// let dateDomain = d3.extent(dates);
-// let s4 = d3.scaleTime().domain(dateDomain).range([0, 400]);
-// let axis4 = d3.axisTop(s4);
 
-// mySelection.append('svg')
-// .attr("width", 500) // Adjust the width based on your layout
-// .attr("height", 50) // Adjust the height based on your layout
-// .append("g")
-// .attr("transform", "translate(20, 30)")
-// .call(axis4);
+
+
+
+
+
+
+
 
 // Create an SVG container for the time bar
 let timeBarSvg = d3.select("div#bar2");
